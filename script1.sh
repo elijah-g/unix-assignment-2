e5fd5c06f407a15a60905f3bef36cd7619e8dbf4
Elijah Glass <s3679959@student.rmit.edu.au>
2018-10-30 20:14:42 +0800

    Add argument control

--- script1.sh
+++ script1.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 #Elijah Glass 
 
-free -h
-echo
 
-df 
+#Check to see if an argument was specified
+if [ -n "$1" ]; then
+	
+	#Create a case for the first argument
+	case "$1" in
 
-echo
+	-memory) echo "**Memory Report**"
+		free -h 
+		;;
 
-ifconfig
+	-disk) echo "**Disk Space Report**"
+		df
+		;;
 
-uptime -p
+	-network) echo "**Network Report**"
+		ifconfig
+		;;
+
+	-uptime) echo "**Uptime Report**"
+		uptime -p
+		;;	
+
+	#Provide the user feedback if the argument was invalid
+	*) echo "Invalid Option"
+
+	esac
+
+#If no agument specified 
+else
+	echo "You must specify a parameter"
+
+fi
+
