b9331647ebed4b7fa67dbcf2d3c71bd92b6d0fd7
Elijah Glass <s3679959@student.rmit.edu.au>
2018-11-04 17:09:41 +0800

    help function for req 2

--- opinfo.sh
+++ opinfo.sh
@@ -1,60 +1,81 @@
 #!/bin/bash
 #Elijah Glass - S3679959
 
-while getopts cprom parm ; do
-	case $parm in
-		c)
-			cores="$(grep -c ^processor /proc/cpuinfo)"
-			echo "Number of CPU cores: $cores" 
-			;;
 
-		p)	
-			#Find the pid of this script using pgrep		
-			pid="$(pgrep -f $0)"
-			
-			#Find it's priority number using ps
-			priority="$(ps --no-headers -o ni $pid)"
+#Function that can be reused to give guidance if an innapropriate argument is given
+function help_required {
+	echo "Usage: sysinfo [-c][-p][-r][-o][-m]"
+	echo
+	echo "-c     the number of cpu cores on the system"
+	echo "-p     the current process' priority (nice number)"
+	echo "-r     the total number of processes running under the current user"
+	echo "-o     the number of open file descriptors owned by the current user"
+	echo "-m     the maximum default number of file descritpors that can be opened by a process"
+	echo
+	echo "For more help read READ.ME"
+}
 
-			#Display the output nicely on the screen
-			echo "Current process nice priority number:$priority"
-			;;
+if [ -n "$1" ]; then
 
-		r)
-			#find the current users username using id
-			user="$(id -u -n)"
+	while getopts cprom parm ; do
+		case $parm in
+			c)
+				cores="$(grep -c ^processor /proc/cpuinfo)"
+				echo "Number of CPU cores: $cores" 
+				;;
 
-			#Count using wc thee number of line of the output of ps for specific user
-			#each process has a line hence number of lines = number of processes
-			num_processes="$(ps --no-headers -U $user | wc -l)"
+			p)	
+				#Find the pid of this script using pgrep		
+				pid="$(pgrep -f $0)"
+				
+				#Find it's priority number using ps
+				priority="$(ps --no-headers -o ni $pid)"
 
-			#Display the output nicely on the screen
-			echo "Number of process for user $user: $num_processes"
-			;;
+				#Display the output nicely on the screen
+				echo "Current process nice priority number:$priority"
+				;;
 
-		o)
-			
-			user="$(id -u -n)"
-			num_fd="$(lsof -a -d 0-999 -u $user | wc -l)"
-			echo "Number of open file descriptors for $user: $num_fd"
-			;;
+			r)
+				#find the current users username using id
+				user="$(id -u -n)"
 
-		m)
-			#find amount of occupied file descriptors
-			occupied="$(cat /proc/sys/fs/file-nr | awk '{print $1;}')"
+				#Count using wc thee number of line of the output of ps for specific user
+				#each process has a line hence number of lines = number of processes
+				num_processes="$(ps --no-headers -U $user | wc -l)"
 
-			#Then find number of total system file descriptors
-			total="$(cat /proc/sys/fs/file-max)"
+				#Display the output nicely on the screen
+				echo "Number of process for user $user: $num_processes"
+				;;
 
-			#calculate available file descriptors. Subtract occupied from total
-			available="$(expr $total - $occupied)"
+			o)
+				
+				user="$(id -u -n)"
+				num_fd="$(lsof -a -d 0-999 -u $user | wc -l)"
+				echo "Number of open file descriptors for $user: $num_fd"
+				;;
 
-			#Display the output nicely on the screen
-			echo "Number of available file descriptors: $available"
-			;;
+			m)
+				#find amount of occupied file descriptors
+				occupied="$(cat /proc/sys/fs/file-nr | awk '{print $1;}')"
 
-		*)
-			echo "inavalid option"
+				#Then find number of total system file descriptors
+				total="$(cat /proc/sys/fs/file-max)"
 
-	esac
-done
+				#calculate available file descriptors. Subtract occupied from total
+				available="$(expr $total - $occupied)"
 
+				#Display the output nicely on the screen
+				echo "Number of available file descriptors: $available"
+				;;
+
+			*)
+				echo "inavalid option"
+
+		esac
+	done
+
+#If no agument specified 
+else
+	echo "No argument was specified"
+	help_required
+if
